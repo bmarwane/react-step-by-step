f1f0f96b956dc19db2d50c0fde24f2867112ab75
Marwane <mbelahcen@octo.com>
2017-07-14 18:03:38 +0100

    Mettre les liens dans le router

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 
 import Home from './pages/Home'
 import About from './pages/About'
@@ -8,6 +10,15 @@ export default function MainRouter () {
     return (
         <Router>
             <div>
+                <Link to='/'>
+                    Home
+                </Link>
+                <br/>
+                <Link to='/about'>
+                    About
+                </Link>
+
+                <hr/>
                 <Route exact path="/" component={Home}/>
                 <Route path="/about" component={About}/>
             </div>
